701fb964708bced4236ca8bb4e857684b68b06d3
rhtyme <rhtyme@gmail.com>
2022-02-25 12:37:23 +0500

    fixed

--- src/main/java/ru/inno/ssdlab06/CurrencyConverter.java
+++ src/main/java/ru/inno/ssdlab06/CurrencyConverter.java
@@ -4,7 +4,7 @@ public class CurrencyConverter {
 
   //TODO your code here
 
-  public Float convert(int amount, String from, String to) {
-    return 10.0;
+  public float convert(int amount, String from, String to) {
+    return 10.f;
   }
 }
