daea5cf806ba1312c689353f67b14f9920148e24
rhtyme <rhtyme@gmail.com>
2022-02-25 12:25:10 +0500

    tests are written

--- src/test/java/ru/inno/ssdlab06/TestCurrencyConverter.java
+++ src/test/java/ru/inno/ssdlab06/TestCurrencyConverter.java
@@ -1,6 +1,8 @@
 package ru.inno.ssdlab06;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,17 +14,30 @@ import java.io.*;
 public class TestCurrencyConverter {
 
    @Test
-   public void testHelloWorld()
+   public void testCurrency()
    {
      PrintStream originalOut = System.out;
      ByteArrayOutputStream bos = new ByteArrayOutputStream();
      System.setOut(new PrintStream(bos));
 
+     CurrencyConverter currency = CurrencyConverter();
+
+     //converting usd to rouble
+
+     Float convertedUsd = currency.convert(1, "rouble", "usd")
+
+     boolean isGreaterThanOne = convertedUsd > 1.0;
+
+     boolean isLessThanTwenty = convertedUsd < 20.0;
+
      // action
-     Main.main(null);
+     // Main.main(null);
+
+     assertTrue(isGreaterThanOne);
+     assertTrue(isLessThanTwenty);
 
      // assertion
-     assertEquals("Hello world!\n", bos.toString());
+     // assertEquals("Hello world!\n", bos.toString());
 
      // undo the binding in System
      System.setOut(originalOut);
